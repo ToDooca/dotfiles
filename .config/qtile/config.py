438cfb487ccf7c40248a6ead145ef8d186a7a332
Dusan Stankovic <todooc4@gmail.com>
2026-03-16 08:42:38 +0100

    reverted the basilisk widget sleeping status logic

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -388,7 +388,7 @@ def get_basilisk_battery_level():
         if charging:
             return _wrap_ok('󰂄')  # charging (bolt)
         if battery_level == 0:
-            return _wrap_ok('󰂄')  # seems like there is no 0% battery state, and instead it's being used when mouse is charging
+            return '󰒲'
         if battery_level > 75:
             return '󱊣'
         if battery_level > 25:
